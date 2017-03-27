6e088f99ff7c3adc334e30dff9ce35febce828da
Prashant Chinchkar <Prashant.Chinchkar@sigma-systems.com>
2017-03-27 15:54:22 +0530

    New project!!

--- server_to_do.js
+++ server_to_do.js
@@ -4,6 +4,25 @@ var app = express();
 
 var PORT = process.env.PORT || 3000;
 
+var todos = [{
+	id: 1,
+	discription: 'Firsrt Case',
+	completed : false
+},{
+	id: 2,
+	discription: '2nd Case',
+	completed : false
+},{
+	id: 3,
+	discription: '3rd Case',
+	completed : true
+}];
+
+app.get('/todos',function(req,res){
+	res.json(todos);
+	res.send('TO DO APLLICATION');
+}); 
+
 
 app.get('/',function(req,res){
 	res.send('TO DO APLLICATION');
